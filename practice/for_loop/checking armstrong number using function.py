5f6025c08e4f35348394b0abdaf5fd5c9fb2089f
manohartanna137 <tanna.manohar@diggibyte.com>
2023-05-13 18:03:51 +0530

    checking armstrong number

---
+++ practice/for_loop/checking armstrong number using function.py
@@ -0,0 +1,20 @@
+def armstrong_number(num):
+    num_copy=num
+    out=0
+    while num>0:
+        rem=num%10
+        out=out+(rem**3)
+        num=num//10
+    if num_copy==out:
+        return 1
+    else:
+        return 0
+armstrong_list=[]
+armstrong_not=[]
+for i in range(1,1001):
+    if (armstrong_number(i) == 1):
+        armstrong_list.append(i)
+    else:
+        armstrong_not.append(i)
+print(armstrong_list)
+print(armstrong_not)
